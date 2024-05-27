ad7673212baaa7fa61ddb4c6cf182f80bee6cd7e
abbasibneakram <abbasibneakram@gmail.com>
2024-05-27 16:44:46 +0500

    implemented jwt middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken')
+
+const jwtAuthMiddleware = (req, res, next) => {
+    const token = req.headers.authorization.split(',')[1]
+    if (!token) return res.status(401).json({ error: 'Unauthorized!' })
+
+    try {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET)
+        req.user = decodedToken
+        next()
+    } catch (err) {
+        console.error(err)
+        res.status(401).json({ error: 'Invalid Token!' })
+    }
+}
+
+module.exports = jwtAuthMiddleware
